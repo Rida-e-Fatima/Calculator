9fb142dd70de5d6adae7bb2ee7c4cb2bb37922f2
Rida-e-Fatima <126771470+Rida-e-Fatima@users.noreply.github.com>
2023-04-11 16:18:45 +0500

    another way

--- calculator.js
+++ calculator.js
@@ -1,18 +1,61 @@
+// function append(val){
+//     document.getElementById("display").value +=val;
+//    }
 
+//     function clear1(){
+//      let cler = " ";
+//      document.getElementById("display").value = cler ;
+//      // document.getElementById("display").value +='';
+
+//     }
+//  function calculate(){
+//  let exp =  document.getElementById("display").value ;
+//   let result = eval(exp);
+//   document.getElementById("display").value = result;
+//  }
 function append(val){
-    document.getElementById("display").value +=val; 
-   }
- 
-    function clear1(){
-     let cler = " ";
-     document.getElementById("display").value = cler ; 
-     // document.getElementById("display").value +='';
- 
+        document.getElementById("display").value +=val;
+       }
+    
+        function clear1(){
+         let cler = " ";
+         document.getElementById("display").value = cler ;
+         // document.getElementById("display").value +='';
+    
+        }
+function calculate() {
+    let expression = document.getElementById("display").value;
+    let operands = expression.split(/[+\-*/]/);
+    let operator = expression.match(/[+\-*/]/);
+  //agr operator  2 lag jayn or ya to operator na ho to ERror dy ga 
+    if (operands.length !== 2 || !operator) {
+      // Invalid expression
+      document.getElementById("display").value = "Error";
+      return;
     }
- function calculate(){
- let exp =  document.getElementById("display").value ;
-  let result = eval(exp);
-  document.getElementById("display").value = result;
- }
- 
-     
+  
+    let a = parseFloat(operands[0]);
+    let b = parseFloat(operands[1]);
+    let res = a+b
+    console.log(res);
+
+    let result;
+  
+    switch (operator[0]){
+      case "+":
+        result = a + b;
+        break;
+      case "-":
+        result = a - b;
+        break;
+      case "*":
+        result = a * b;
+        break;
+      case "/":
+        result = a / b;
+        break;
+    }
+  
+    document.getElementById("display").value = result;
+  }
+  
